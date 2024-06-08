8ba0ac91f265849269e112b2b53caabacbdeda60
John Marco Condino <johnmcondino@gmail.com>
2024-06-08 13:40:06 +0800

    feat: build header genre dropdown component

--- components/GenreDropdown.tsx
+++ components/GenreDropdown.tsx
@@ -1,4 +1,14 @@
 import { Genres } from "@/typings";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
+import Link from "next/link";
 
 const GenreDropdown = async () => {
   const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
@@ -23,9 +33,22 @@ const GenreDropdown = async () => {
 
 
   return (
-    <div>
-      GenreDropdown
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger className="text-white flex justify-center items-center">
+        Genre
+        <ChevronDown className="ml-1"/>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Select a Genre</DropdownMenuLabel>
+        <DropdownMenuSeparator/>
+
+        {data.genres.map(genre => (
+          <DropdownMenuItem key={genre.id}>
+            <Link href={`/genre/${genre.id}?genre=${genre.name}`}>{genre.name}</Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
