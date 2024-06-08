82b40f8d94dde75c21dedf86654dbf8fdbb05e3c
John Marco Condino <johnmcondino@gmail.com>
2024-06-08 16:11:22 +0800

    update: add missing type in typings.ts

--- typings.ts
+++ typings.ts
@@ -5,6 +5,7 @@ export type Movie = {
   id: number;
   original_language: string;
   original_title: string;
+  overview: string;
   popularity: number;
   poster_path?: string;
   release_date: string;
@@ -12,20 +13,20 @@ export type Movie = {
   video: boolean;
   vote_average: number;
   vote_count: number;
-}
+};
 
 export type SearchResults = {
   page: number;
   results: Movie[];
   total_pages: number;
   total_results: number;
-}
+};
 
 export type Genre = {
   id: number;
   name: string;
-}
+};
 
 export type Genres = {
   genres: Genre[];
-}
+};
