dd43163807f429a86b927dc8538e59bfc54fb4c3
John Marco Condino <johnmcondino@gmail.com>
2024-06-08 14:07:27 +0800

    feat: building movie carousel and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 import MovieCarousel from "@/components/MovieCarousel";
+import { getPopularMovies, getTopRatedMovies, getUpcomingMovies } from "@/lib/getMovies";
 
 const Home = async () => {
   const upcomingMovies = await getUpcomingMovies();
-  const topRatedMovies = await gettopRatedMovies();
-  const popularMovies = await getpopularMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
 
   return (
     <main className="">
       {/*<CarouselBannerWrapper/>*/}
 
       <div className="flex flex-col space-y-2 xl:-mt-48">
-        <MovieCarousel movies={[]} title="Upcoming" />
-        {/*<MovieCarousel movies={} title={} />*/}
-        {/*<MovieCarousel movies={} title={} />*/}
+        <MovieCarousel movies={upcomingMovies} title="Upcoming" />
+        <MovieCarousel movies={topRatedMovies} title="Top Rated" />
+        <MovieCarousel movies={popularMovies} title="Popular Movies" />
       </div>
     </main>
   );
